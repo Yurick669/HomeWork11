797c010e543f8017453b76675ce241956e677ff6
Home <yulaguby@gmail.com>
2022-02-01 17:31:03 +0300

    Initial commit

---
+++ src/test/java/com/example/HomeWork11/HomeWork11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeWork11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
